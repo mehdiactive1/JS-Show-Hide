cddfea4a80e9b960c875a973b8cd0b13e40eb68f
Aida Rostami <aidarostami1992@gmail.com>
2019-03-05 00:25:57 +0330

    Add files via upload

--- js-test.js
+++ js-test.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $(".hs-j2,.hs-j3").hide();
 });
 
+/*
 $(".bullet1").click(function(){
   $(".hs-j1").show();
   $(".hs-j2").hide();
@@ -17,12 +18,26 @@ $(".bullet3").click(function(){
   $(".hs-j3").show();
   $(".hs-j2").hide();
   $(".hs-j1").hide();
+});*/
+
+//slideshow (show,hide)
+jQuery(function(){
+        jQuery('.bullets').click(function(){
+              jQuery('.hide-js').hide();
+              jQuery('#div'+$(this).attr('target')).show();
+        });
 });
 
+//fadeeffect
+$(".bullets").click(function(){
+  $(".hide-js").fadeIn();
+});
+
+//mousemove effect
 $(document).ready(function(){
   $('.hs-j1').mousemove(function(e){
-        var x = e.pageX - this.offsetLeft;
-        var y = e.pageY - this.offsetTop;
+        var x = e.pageX - this.offsetLeft - 300;
+        var y = e.pageY - this.offsetTop - 300;
         $('.hugetext').css({'top': x,'left': y}); 
   });
 });
